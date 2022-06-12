a7f1a9747d5f769e2e5967662f99fe76046c5e66
soiya-kun <djmaajtj@gmail.com>
2022-06-12 19:35:25 +0900

    株価の表示実装

---
+++ app/interface/stock.ts
@@ -0,0 +1,16 @@
+export interface StockResponse {
+  sc: string
+  name: string
+  market: string
+  industry: string
+  b_date: string
+  opened_price: number
+  high_price: number
+  low_price: number
+  closed_price: number
+  volume: number
+  transaction_price: number
+  market_capitalization: number
+  low_limit: number
+  high_limit: number
+}
